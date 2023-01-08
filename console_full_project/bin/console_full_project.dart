744a05cebb6aceb12a410c1ceb75ccfe9d57c9f3
Anant Dubey <anantkumar312@gmail.com>
2023-01-08 20:31:10 +0530

    first dart project

---
+++ console_full_project/bin/console_full_project.dart
@@ -0,0 +1,5 @@
+import 'package:console_full_project/src/calculate.dart';
+
+void main() {
+  print('Add: ' + calculateAdd().toString());
+}
---
+++ console_full_project/lib/src/calculate.dart
@@ -0,0 +1,7 @@
+import 'package:calculator/calculator.dart';
+
+import 'package:calculator/calculator.dart';
+
+int calculateAdd() {
+  return add(4, 8);
+}
