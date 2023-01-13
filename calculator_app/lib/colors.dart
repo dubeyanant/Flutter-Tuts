bfe1f874b736e6e9f592834b412920dcbb48012d
Anant Dubey <anantkumar312@gmail.com>
2023-01-13 19:43:02 +0530

    added colors.dart

---
+++ calculator_app/lib/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+const operatorColor = Color(0xFF272727);
+const buttonColor = Color(0xFF191919);
+const orangeColor = Color(0xFFD9802E);
